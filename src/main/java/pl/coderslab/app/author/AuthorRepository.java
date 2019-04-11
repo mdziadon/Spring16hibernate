ad03df0fcc51a64207af9aa1239dfd7b03c080ba
Marcin Dziadon <marcin.dziadon@coderslab.pl>
2019-04-11 12:42:08 +0200

    Zadania dzien 4

---
+++ src/main/java/pl/coderslab/app/author/AuthorRepository.java
@@ -0,0 +1,18 @@
+package pl.coderslab.app.author;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    Author findByEmail(String email);
+
+    Author findByPesel(String pesel);
+
+    List<Author> findByLastName(String lastName);
+    
+
+}
